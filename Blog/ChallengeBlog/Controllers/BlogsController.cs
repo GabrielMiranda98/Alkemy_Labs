0b86aa2e523d06e8b01bb060f41e6ac1702409b2
Gabriel Miranda <60537615+GabrielMiranda98@users.noreply.github.com>
2020-11-03 13:57:29 -0300

    Clase Blog y Views con Scaffolding

---
+++ Blog/ChallengeBlog/Controllers/BlogsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChallengeBlog.Models;
+
+namespace ChallengeBlog.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private blogEntities db = new blogEntities();
+
+        // GET: Blogs
+        public ActionResult Index()
+        {
+            return View(db.Blogs.ToList());
+        }
+
+        // GET: Blogs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blogs blogs = db.Blogs.Find(id);
+            if (blogs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogs);
+        }
+
+        // GET: Blogs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Blogs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Titulo,Contenido,Categoria,fechaDeCreacion")] Blogs blogs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Blogs.Add(blogs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blogs);
+        }
+
+        // GET: Blogs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blogs blogs = db.Blogs.Find(id);
+            if (blogs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogs);
+        }
+
+        // POST: Blogs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Titulo,Contenido,Categoria,fechaDeCreacion")] Blogs blogs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blogs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blogs);
+        }
+
+        // GET: Blogs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blogs blogs = db.Blogs.Find(id);
+            if (blogs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogs);
+        }
+
+        // POST: Blogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Blogs blogs = db.Blogs.Find(id);
+            db.Blogs.Remove(blogs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
